7b5089290aa4c9b801579e128558128977e76781
ataumo <l.coumau@yahoo.fr>
2019-11-26 01:47:59 +0100

    project

--- check_parser_results.py
+++ check_parser_results.py
@@ -29,6 +29,102 @@ def test_file_parser():
             exit(-1)
     print("sucess !!")
 
+def test(name,run,enter,expect):
+    print('\n')
+    print("test",name,': ', end='')
+    result = subprocess.Popen(run,stdin = subprocess.PIPE, encoding = 'utf8',stdout=subprocess.DEVNULL)
+    result.stdin.write(enter)
+    result.stdin.close()
+    result.wait()
+    if expect == 'success':
+        if (result.returncode == 0):
+            print("pass-------------------------------------------------")
+        else:
+            print("fail------------------------------------------------X")
+    else:
+        if (result.returncode == 0):
+            print("fail------------------------------------------------X")
+        else :
+            print("pass-------------------------------------------------")
+
+def test_serie():
+    test(1,["./reversi"],'Q\n\n','success')
+    test(2,["./reversi",'-x'],'Q\n\n','')
+    test(3,["./reversi",'-v'],'Q\n\n','success')
+    test(4,["./reversi",'--verbose'],'Q\n\n','success')
+    test(5,["./reversi",'-V'],'Q\n\n','success')
+    test(6,["./reversi",'--version'],'Q\n\n','success')
+    test(7,["./reversi",'-h'],'Q\n\n','success')
+    test(8,["./reversi",'--help'],'Q\n\n','success')
+    test(9,["./reversi",'--he'],'Q\n\n','success')
+
+    test(10,["./reversi",'-b'],'Q\n\n','success')
+    test(11,["./reversi",'--black-ai'],'Q\n\n','success')
+    test(12,["./reversi",'-b0'],'Q\n\n','success')
+    test(13,["./reversi",'--black-ai=0'],'Q\n\n','success')
+    test(14,["./reversi",'-b1'],'Q\n\n','success')
+    test(15,["./reversi",'--black-ai=1'],'Q\n\n','success')
+
+    test(16,["./reversi",'-w'],'Q\n\n','success')
+    test(17,["./reversi",'--white-ai'],'Q\n\n','success')
+    test(18,["./reversi",'-w0'],'Q\n\n','success')
+    test(19,["./reversi",'--white-ai=0'],'Q\n\n','success')
+    test(20,["./reversi",'-w1'],'Q\n\n','success')
+    test(21,["./reversi",'--white-ai=1'],'Q\n\n','success')
+
+    test(22,["./reversi",'-b','-w'],'Q\n\n','success')
+    test(23,["./reversi",'--black-ai','--white-ai'],'Q\n\n','success')
+    test(24,["./reversi",'-b0','-w1'],'Q\n\n','success')
+    test(25,["./reversi",'--black-ai=0','--white-ai=1'],'Q\n\n','success')
+
+    test(26,["./reversi",'-s'],'Q\n\n','')
+    test(27,["./reversi",'--size'],'Q\n\n','')
+    test(28,["./reversi",'-s','1'],'Q\n\n','success')
+    test(29,["./reversi",'--size','1'],'Q\n\n','success')
+    test(30,["./reversi",'-s','2'],'Q\n\n','success')
+    test(31,["./reversi",'--size','2'],'Q\n\n','success')
+    test(32,["./reversi",'-s','3'],'Q\n\n','success')
+    test(33,["./reversi",'--size','3'],'Q\n\n','success')
+    test(34,["./reversi",'-s','4'],'Q\n\n','success')
+    test(35,["./reversi",'--size','4'],'Q\n\n','success')
+    test(36,["./reversi",'-s','5'],'Q\n\n','success')
+    test(37,["./reversi",'--size','5'],'Q\n\n','success')
+    test(38,["./reversi",'-s','0'],'Q\n\n','')
+    test(39,["./reversi",'--size','0'],'Q\n\n','')
+    test(38,["./reversi",'-s','6'],'Q\n\n','')
+    test(39,["./reversi",'--size','6'],'Q\n\n','')
+
+    #argument parser
+    test(40,["./reversi",'board.txt'],'Q\n\n','success')
+    test(41,["./reversi",'-v','board.txt'],'Q\n\n','success')
+    test(42,["./reversi",'--verbose','board.txt'],'Q\n\n','success')
+    test(43,["./reversi",'-c'],'Q\n\n','')
+    test(44,["./reversi",'--contest'],'Q\n\n','')
+    test(45,["./reversi",'-c','missing.txt'],'Q\n\n','')
+    test(46,["./reversi",'--contest','missing.txt'],'Q\n\n','')
+    test(47,["./reversi",'-c','shadow'],'Q\n\n','')
+    test(48,["./reversi",'--contest','shadow'],'Q\n\n','')
+    test(49,["./reversi",'-c','board.txt'],'Q\n\n','success')
+    test(50,["./reversi",'--contest','board.txt'],'Q\n\n','success')
+    test(51,["./reversi",'-c','-v','board.txt'],'Q\n\n','success')
+    test(52,["./reversi",'--contest','--verbose','board.txt'],'Q\n\n','success')
+
+    #file parser
+    i=52
+    for f in listdir("board_parser/"):
+        i=i+1
+        filename = "board_parser/"+f
+        print('\n'+f)
+        if ("pass" in f):
+            test(i,["./reversi",filename],'Q\n\n','success')
+        else:
+            test(i,["./reversi",filename],'Q\n\n','')
+
+
+
+
+test_serie()
+
 def test_game_time(n,depth):
     ticks = time.time()
     L=[]
@@ -86,4 +182,4 @@ def check_test():
         print(result.returncode)
 
 #check_test()
-test_game_time(1000,5)
+#test_game_time(1000,5)
